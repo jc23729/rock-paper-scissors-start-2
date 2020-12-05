2520c9490f1e4c4e1e479e58f5b3292f1bd7e4af
Juan Cruz <jc23729@gmail.com>
2020-12-05 16:54:00 +0000

    fixed the index error where if you typed a number out of range then the code will break.

--- main.py
+++ main.py
@@ -28,16 +28,16 @@ scissors = '''
 ---.__(___)
 '''
 game_images = [rock, paper, scissors]
+
 user_choice= int(input("What would you like to choose? Type 0 for Rock, 1 for Paper, 2 for Scissors\n"))
 print(game_images[user_choice])
 
 computer_choice= random.randint(0,2)
 print("Computer chose:")
 print(game_images[computer_choice])
-
+# this choice will take care of everything out range out of 0-2 
 if user_choice>=3  or user_choice <0:
   print("You chose an invalid numbe, YOU LOSE")
-
 elif user_choice == 0 and computer_choice == 2:
   print("You win")
 elif computer_choice ==0 and user_choice == 2:
