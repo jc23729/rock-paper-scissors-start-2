5d585d597960b6c0e845f36f43c3e921400e5cb4
Juan Cruz <jc23729@gmail.com>
2020-12-05 16:58:12 +0000

    modified the code, moved theif user_choice>=3  or user_choice <0: to fix the index out of range error but it up top so the progam could read it first

--- main.py
+++ main.py
@@ -30,21 +30,24 @@ scissors = '''
 game_images = [rock, paper, scissors]
 
 user_choice= int(input("What would you like to choose? Type 0 for Rock, 1 for Paper, 2 for Scissors\n"))
-print(game_images[user_choice])
 
-computer_choice= random.randint(0,2)
-print("Computer chose:")
-print(game_images[computer_choice])
-# this choice will take care of everything out range out of 0-2 
 if user_choice>=3  or user_choice <0:
   print("You chose an invalid numbe, YOU LOSE")
-elif user_choice == 0 and computer_choice == 2:
-  print("You win")
-elif computer_choice ==0 and user_choice == 2:
-  print("You lose")
-elif computer_choice > user_choice:
-  print("You lose")
-elif user_choice > computer_choice:
-  print("You win")
-elif computer_choice == user_choice:
-  print("Its a draw")
+else:
+  print(game_images[user_choice])
+
+  computer_choice= random.randint(0,2)
+  print("Computer chose:")
+  print(game_images[computer_choice])
+  # this choice will take care of everything out range out of 0-2 
+
+  if user_choice == 0 and computer_choice == 2:
+    print("You win")
+  elif computer_choice ==0 and user_choice == 2:
+    print("You lose")
+  elif computer_choice > user_choice:
+    print("You lose")
+  elif user_choice > computer_choice:
+    print("You win")
+  elif computer_choice == user_choice:
+    print("Its a draw")
